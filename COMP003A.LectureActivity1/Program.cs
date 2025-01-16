f8d28970c709e930a5da87a7738f740f4210aace
Colby Domingos <colbydomingos@my.whccd.edu>
2025-01-15 20:14:33 -0800

    added all neccessary identifing information

--- COMP003A.LectureActivity1/Program.cs
+++ COMP003A.LectureActivity1/Program.cs
@@ -1,4 +1,11 @@
-﻿//Namespace Level
+﻿/* 
+    Author: Colby Domingos
+    Course: COMP-003A
+    Faculty: Jonathan Rodrigo Cruz
+    Purpose: Basic console app
+ */
+
+//Namespace Level
 namespace COMP003A.LectureActivity1
 {
     //Class Level
